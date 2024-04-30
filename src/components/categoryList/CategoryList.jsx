fc254129d3fa86be78d9503ba932dbe8b679744e
AdakHaddad <muhammadmuqtada@gmail.com>
2024-04-30 10:05:21 +0700

    first commit

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,44 +1,56 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
-    cache: "no-store",
-  });
+const CategoryList = () => {
+  const [data, setData] = useState([]);
 
-  if (!res.ok) {
-    throw new Error("Failed");
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/categories", {
+          cache: "no-store",
+        });
 
-  return res.json();
-};
+        if (!res.ok) {
+          throw new Error("Failed to fetch categories");
+        }
 
-const CategoryList = async () => {
-  const data = await getData();
+        const categories = await res.json();
+        setData(categories.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        {data?.map((item) => (
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles[item.slug]}`}
-            key={item._id}
-          >
-            {item.img && (
-              <Image
-                src={item.img}
-                alt=""
-                width={32}
-                height={32}
-                className={styles.image}
-              />
-            )}
-            {item.title}
-          </Link>
-        ))}
+        {Array.isArray(data) &&
+          data.map((item) => (
+            <Link
+              href={`/blog?cat=${item.slug}`}
+              className={`${styles.category} ${styles[item.slug]}`}
+              key={item._id}
+            >
+              {item.img && (
+                <Image
+                  src={item.img}
+                  alt=""
+                  width={32}
+                  height={32}
+                  className={styles.image}
+                />
+              )}
+              {item.title}
+            </Link>
+          ))}
       </div>
     </div>
   );
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "blog-cddc2.firebaseapp.com",
-  projectId: "blog-cddc2",
-  storageBucket: "blog-cddc2.appspot.com",
-  messagingSenderId: "413696194974",
-  appId: "1:413696194974:web:bc0a551cd5558bdf0dbe8f"
+  authDomain: "intracker-base.firebaseapp.com",
+  projectId: "intracker-base",
+  storageBucket: "intracker-base.appspot.com",
+  messagingSenderId: "633294640208",
+  appId: "1:633294640208:web:245dc200e69fbce08b6d55",
+  measurementId: "G-TZM9SKR1RY",
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
