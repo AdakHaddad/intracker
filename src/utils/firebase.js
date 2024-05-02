9e3162f8f2138783890851c5b544a95eab47847e
AdakHaddad <muhammadmuqtada@gmail.com>
2024-05-02 21:00:32 +0700

    Firebase Api ga harus di .env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE,
+  apiKey: "AIzaSyAQX3H-PUEj1AUyO-X8vmBdArveo3UMNi8",
   authDomain: "intracker-base.firebaseapp.com",
   projectId: "intracker-base",
   storageBucket: "intracker-base.appspot.com",
@@ -16,5 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
