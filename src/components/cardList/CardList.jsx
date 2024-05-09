8d9c6e01227726849d05a0c8a648ab02615ccf6e
AdakHaddad <muhammadmuqtada@gmail.com>
2024-05-09 13:06:31 +0700

    CardList debug

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import styles from "./cardList.module.css";
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./CardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Image from "next/image";
 import Card from "../card/Card";
 
+const host = process.env.NEXTAUTH_URL || "http://localhost:3000";
+
 const getData = async (page, cat) => {
-  const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
-    {
-      cache: "no-store",
-    }
-  );
+  const res = await fetch(`${host}/api/posts?page=${page}&cat=${cat || ""}`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed");
@@ -19,11 +19,24 @@ const getData = async (page, cat) => {
   return res.json();
 };
 
-const CardList = async ({ page, cat }) => {
-  const { posts, count } = await getData(page, cat);
+const CardList = ({ page, cat }) => {
+  const [posts, setPosts] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { posts, count } = await getData(page, cat);
+        setPosts(posts);
+        setCount(count);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+    fetchData();
+  }, [page, cat]);
 
   const POST_PER_PAGE = 2;
-
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
   const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
@@ -31,9 +44,14 @@ const CardList = async ({ page, cat }) => {
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        {posts?.map((item) => (
-          <Card item={item} key={item._id} />
-        ))}
+        {posts?.map(
+          (
+            item,
+            index // Add key prop here
+          ) => (
+            <Card key={index} item={item} /> // Use unique identifier as key, here I'm using index
+          )
+        )}
       </div>
       <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
